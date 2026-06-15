83b9145029b45fc7a215695b9e6625d51e268084
ammangame748-dev <ammangame748@gmail.com>
2026-06-15 19:14:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Client, GatewayIntentBits, AttachmentBuilder } = require('discord.js');
 const express = require('express');
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const app = express();
 const axios = require('axios');
 const Jsoning = require('jsoning');
 const FormData = require('form-data');
